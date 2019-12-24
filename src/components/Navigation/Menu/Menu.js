a00c595c77406c05eb81a4dbbeccf7bfb8b3a983
Susan Subedi <susansubedi34@gmail.com>
2019-12-24 15:44:00 -0600

    Higher Order Components/ Lifecycle Method Done

--- src/components/Navigation/Menu/Menu.js
+++ src/components/Navigation/Menu/Menu.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classes from "./Menu.module.css";
 
 const menu = props => {
-  return <div onClick={props.clicked}>Menu</div>;
+  return (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default menu;
